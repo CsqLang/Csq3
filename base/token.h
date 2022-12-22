7ba20682832965bfafe689097d282b2badb7f107
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2022-12-22 15:33:21 +0530

    Update token.h

--- base/token.h
+++ base/token.h
@@ -25,7 +25,6 @@
         "__divassign__",
         "__increment__",
         ":=",
-        "#",
     },
     {   
         "auto operator +",
@@ -49,7 +48,6 @@
         "auto operator /=",
         "auto operator ++",
         "=",
-        "//",
     }
     };
 
